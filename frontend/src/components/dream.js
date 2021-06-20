5259aefedfb8f1f7046d6d72e99eaaa27464d449
Pbatch <peter_batchelor1@hotmail.com>
2021-06-20 21:27:01 +0100

    Moved image rendering inside useEffect hook

--- frontend/src/components/dream.js
+++ frontend/src/components/dream.js
@@ -4,44 +4,44 @@ import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 import { WAIT_FOR_PLAYERS, SELECT_IMAGE, END_OF_GAME } from '../constants/modes'
 import { CreateMessage } from '../graphql/mutations'
 import { API, graphqlOperation } from 'aws-amplify'
+import { useEffect, useState } from 'react'
 
 const Dream = () => {
   const dispatch = useDispatch()
   const state = useSelector(state => state, shallowEqual)
+  const [imageDisplay, setImageDisplay] = useState()
 
-  const handleClick = async (e) => {
-    e.preventDefault()
-    if (state.mode !== SELECT_IMAGE) return
-    const newImages = [{'url': e.target.src,
-                        'username': state.username,
-                        'prompt': state.prompt
-                       }]
-    const message = {url: e.target.src, 
-                     round: state.round + 1, 
-                     username: state.username, 
-                     roomName: state.roomName}
-    API.graphql(graphqlOperation(CreateMessage, message))
-    dispatch(setImages(newImages))
-    dispatch(setMode(WAIT_FOR_PLAYERS))
-  }
-
-  const imageStyle = {...imageStyle_, width: `${80/(state.images.length + 2)}vw`}
-
-  const images = state.images.map(({ url, username, prompt }) => {
-    if (!prompt) return <div key={v4()}></div>
-    const cleanPrompt = prompt.replaceAll(/_/g," ")
-    const caption = (state.mode === END_OF_GAME) ? `${username}: "${cleanPrompt}"` : '' 
-    return (
-      <div key={v4()}>
-        <img src={url} alt={url} onClick={handleClick} style={imageStyle} />
-        <div style={captionStyle}>{caption}</div>
-      </div>
-      )
-    })
+  useEffect(() => {
+    const handleClick = async (e) => {
+      e.preventDefault()
+      if (state.mode !== SELECT_IMAGE) return
+      const newImages = state.images.filter((image) => image.url === e.target.src)
+      const message = {url: e.target.src, 
+                       round: state.round + 1, 
+                       username: state.username, 
+                       roomName: state.roomName}
+      API.graphql(graphqlOperation(CreateMessage, message))
+      dispatch(setImages(newImages))
+      dispatch(setMode(WAIT_FOR_PLAYERS))
+    }
+    const imageStyle = {...imageStyle_, width: `${80/(state.images.length + 2)}vw`}
+    const newImageDisplay = state.images.map(({ url, username, prompt }) => {
+      if (!prompt) return <div key={v4()}></div>
+      const cleanPrompt = prompt.replaceAll(/_/g," ")
+      const caption = (state.mode === END_OF_GAME) ? `${username}: "${cleanPrompt}"` : '' 
+      return (
+        <div key={v4()}>
+          <img src={url} alt={url} onClick={handleClick} style={imageStyle} />
+          <div style={captionStyle}>{caption}</div>
+        </div>
+        )
+      })
+      setImageDisplay(newImageDisplay)
+  }, [state, dispatch])
  
   return (
     <div style={dreamStyle}>
-      {images}
+      {imageDisplay}
     </div>
   )
 }
