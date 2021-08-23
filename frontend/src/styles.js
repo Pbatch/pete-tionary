633f6c394f8c2430407d51cfe2228884a5a25098
Pbatch <peter_batchelor1@hotmail.com>
2021-08-23 01:22:20 +0100

    Added styles for buttons and for font

---
+++ frontend/src/styles.js
@@ -0,0 +1,19 @@
+export const styles = {
+  'font': {
+    color: 'white',
+    fontFamily: 'Arial',
+    textAlign: 'center',
+    fontSize: '1em'
+  },
+  'button': {
+    color: 'white',
+    fontFamily: 'Arial',
+    textAlign: 'center',
+    fontSize: '1em',
+    backgroundColor: '#375a7f',
+    borderRadius: '8px',
+    ':hover': {
+      backgroundColor: '#204d74',
+    }
+  }
+}
