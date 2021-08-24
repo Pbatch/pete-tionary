da72d70ad7e533d537641ff81baf7523d378f2ea
Pbatch <peter_batchelor1@hotmail.com>
2021-08-25 00:25:32 +0100

    Restyling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,11 @@ import Amplify from 'aws-amplify'
 import { AMPLIFY_CONFIG } from './constants/config'
 import configureStore from './configure-store'
 import { Provider } from 'react-redux'
+import { LIGHT_GRAY } from './constants/colours'
 
 Amplify.configure(AMPLIFY_CONFIG)
 const store = configureStore()
-document.body.style = 'background: #333; margin: 0'
+document.body.style = `background: ${LIGHT_GRAY}; margin: 0`
 
 ReactDOM.render(
   <Provider store={store}>
