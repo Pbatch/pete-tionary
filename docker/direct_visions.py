8040d04984a994dfc13502c50a0c651fd6c36a4a
Pbatch <peter_batchelor1@hotmail.com>
2021-09-28 12:33:00 +0100

    Removed kornia, added enhancer, lowered cycles and cuts

--- docker/direct_visions.py
+++ docker/direct_visions.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-import kornia.augmentation as K
-from torchvision.transforms import ToPILImage
+from torchvision.transforms import ToPILImage, RandomResizedCrop, Normalize, Resize
 from tqdm import tqdm
-from kornia.geometry.transform import Resize
 from tokenizer import tokenize
 
 
@@ -14,10 +12,11 @@ class DirectVisions:
     def __init__(self,
                  prompt,
                  perceptor,
+                 enhancer,
                  noise_std=0.2,
                  n_images=1,
-                 cycles=500,
-                 cuts=2,
+                 cycles=250,
+                 cuts=1,
                  gamma=1.0,
                  gain=1.0,
                  chroma_noise_scale=1.0,
@@ -29,6 +28,7 @@ class DirectVisions:
                  ):
         self.prompt = prompt
         self.perceptor = perceptor
+        self.enhancer = enhancer
         self.noise_std = noise_std
         self.n_images = n_images
         self.cycles = cycles
@@ -50,19 +50,26 @@ class DirectVisions:
 
         self.prompt_embed = None
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.augs = torch.nn.Sequential(
+            Resize(self.PERCEPTOR_SIZE),
+            RandomResizedCrop(size=(self.PERCEPTOR_SIZE, self.PERCEPTOR_SIZE),
+                              scale=(0.1, 0.9)),
+            Normalize(mean=(0.48145466, 0.4578275, 0.40821073),
+                      std=(0.26862954, 0.26130258, 0.27577711)),
+        )
 
         torch.autograd.set_grad_enabled(False)
         torch.backends.cudnn.benchmark = True
 
     @staticmethod
-    def _calculate_total_variation_loss(image, strength):
-        Y = (image[:, :, 1:, :] - image[:, :, :-1, :]).abs().mean()
-        X = (image[:, :, :, 1:] - image[:, :, :, :-1]).abs().mean()
+    def _calculate_total_variation_loss(images, strength):
+        Y = (images[:, :, 1:, :] - images[:, :, :-1, :]).abs().mean()
+        X = (images[:, :, :, 1:] - images[:, :, :, :-1]).abs().mean()
         loss = (X + Y) * 0.5 * strength
         return loss
 
     @staticmethod
-    def _luma_chroma_to_image(luma, chroma):
+    def _luma_chroma_to_images(luma, chroma):
         luma = torch.sigmoid(luma)
         chroma = torch.sigmoid(chroma) - 0.5
 
@@ -72,20 +79,8 @@ class DirectVisions:
         R = luma + Co - Cg
         G = luma + Cg
         B = luma - Co - Cg
-        image = torch.cat((R, G, B), dim=1)
-        return image
-
-    @staticmethod
-    def _image_to_luma_chroma(image):
-        R, G, B = image[:, 0:1], image[:, 1:2], image[:, 2:3]
-        luma = R * 0.25 + G * 0.5 + B * 0.25
-        Co = R * 0.5 - B * 0.5
-        Cg = - R * 0.25 + G * 0.5 - B * 0.25
-
-        chroma = torch.cat([Co, Cg], dim=1)
-        chroma = torch.logit((chroma + 0.5), eps=1e-6)
-        luma = torch.logit(luma, eps=1e-6)
-        return luma, chroma
+        images = torch.cat((R, G, B), dim=1)
+        return images
 
     def _embed_prompt(self):
         tokenized_prompt = tokenize(self.prompt).to(self.device)
@@ -107,48 +102,39 @@ class DirectVisions:
 
         return luma, chroma
 
-    def _calculate_clip_loss(self, image):
-        augs = torch.nn.Sequential(
-            Resize(self.PERCEPTOR_SIZE),
-            K.RandomResizedCrop(size=(self.PERCEPTOR_SIZE, self.PERCEPTOR_SIZE),
-                                scale=(0.1, 1.0),
-                                align_corners=False),
-            K.Normalize(mean=(0.48145466, 0.4578275, 0.40821073),
-                        std=(0.26862954, 0.26130258, 0.27577711)),
-            K.RandomGaussianNoise(std=self.noise_std, p=1.0)
-        )
-        image_batch = augs(image.repeat(self.cuts, 1, 1, 1))
+    def _calculate_clip_loss(self, images):
+        image_batch = self.augs(images.repeat(self.cuts, 1, 1, 1))
+        image_batch += torch.randn_like(image_batch) * self.noise_std
         image_embed = self.perceptor.encode_image(image_batch)
 
         loss = torch.mean(-torch.cosine_similarity(self.prompt_embed, image_embed))
 
         return loss
 
-    def _calculate_loss(self, image, denoise):
+    def _calculate_loss(self, images, denoise):
         loss = 0
-        loss += self._calculate_clip_loss(image)
-        loss += self._calculate_total_variation_loss(image, denoise)
+        loss += self._calculate_clip_loss(images)
+        loss += self._calculate_total_variation_loss(images, denoise)
         return loss
 
     def _cycle(self, luma, chroma, denoise, optimizer):
         with tqdm(total=self.cycles) as pbar:
             for _ in range(self.cycles):
                 with torch.enable_grad():
-                    image = self._luma_chroma_to_image(luma, chroma)
-                    loss = self._calculate_loss(image, denoise)
+                    images = self._luma_chroma_to_images(luma, chroma)
+                    loss = self._calculate_loss(images, denoise)
                     pbar.update(1)
                     pbar.set_description(f'Loss:{loss:.3f}')
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad(set_to_none=True)
 
-    def _save_image(self, image):
-        image = image.cpu().float().clamp(0., 1.)
-        for i in range(image.shape[0]):
+    def _save_images(self, images):
+        for i in range(self.n_images):
             save_path = f'prompt={self.prompt.replace(" ", "_")}-seed={i}.jpg'
-            pil_image = ToPILImage()(image[i].squeeze())
-            pil_image = pil_image.resize((256, 256))
-            pil_image.save(save_path, quality=95, subsampling=0)
+            image = self.enhancer(images[i:i + 1]).data.squeeze().float().cpu().clamp_(0, 1)
+            image = ToPILImage()(image.squeeze())
+            image.save(save_path, quality=95, subsampling=0)
 
     def run(self):
         dim = [4, 8, 16, 32, 64]
@@ -185,8 +171,8 @@ class DirectVisions:
             optimizer = torch.optim.AdamW(params)
 
             self._cycle(luma, chroma, denoise[i], optimizer)
-            image = self._luma_chroma_to_image(luma, chroma)
+            images = self._luma_chroma_to_images(luma, chroma)
 
             # On the last iteration, save the images
             if i == len(dim) - 1:
-                self._save_image(image)
+                self._save_images(images)
